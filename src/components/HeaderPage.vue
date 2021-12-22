de2b4c86d0ae8aa92be7913c0a18cf5337814f41
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-12-22 17:15:17 +0900

    211222_header(1)

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,5 +1,17 @@
 <template>
-<div>Header</div>
+<div class="header_wrap">
+  <div class="header content_center">
+    <div class="logo_wrap"></div>
+    <ul class="menu_wrap">
+      <li class="menu_li pointer">브랜드</li>
+      <li class="menu_li pointer">도시락</li>
+      <li class="menu_li pointer">푸드트럭</li>
+      <li class="menu_li pointer">케이터링</li>
+      <li class="menu_li pointer">셰프이야기</li>
+      <li class="menu_li menu_li_bg pointer">주문내역</li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
