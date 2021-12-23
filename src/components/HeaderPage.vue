7a29b73feab1fbc1f353d759f8c8bee8d42e062a
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-12-23 12:44:44 +0900

    211223_main(1)

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,23 @@
 <template>
 <div class="header_wrap">
+  <ul class="head_top">
+    <li class="head_top_li pointer">
+      로그인
+    </li>
+    <li class="head_top_li pointer">
+      회원가입
+    </li>
+    <li class="head_top_li pointer">
+      입점문의
+    </li>
+    <li class="head_top_li pointer">
+      고객센터
+    </li>
+  </ul>
   <div class="header content_center">
-    <div class="logo_wrap"></div>
+    <div class="logo_wrap pointer">
+      <img src="../assets/image/logo.png" alt="logo">
+    </div>
     <ul class="menu_wrap">
       <li class="menu_li pointer">브랜드</li>
       <li class="menu_li pointer">도시락</li>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,63 @@
 <template>
-<div>Home</div>
+  <div class="main_wrap">
+    <div class="main_sec1">
+      <swiper class="swiper" :options="swiperOption">
+        <swiper-slide>
+          <div class="main_bg">
+            <img src="../assets/image/main_visual_img03.jpg" alt="main">
+          </div>
+          <div class="main_txt">
+            <img src="../assets/image/main_visual_pc_txt01.png" alt="main">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="main_bg">
+            <img src="../assets/image/main_visual_img02.jpg" alt="logo">
+          </div>
+          <div class="main_txt">
+            <img src="../assets/image/main_visual_pc_txt02.png" alt="main">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="main_bg">
+            <img src="../assets/image/main_visual_img01.jpg" alt="logo">
+          </div>
+          <div class="main_txt">
+            <img src="../assets/image/main_visual_pc_txt03.png" alt="main">
+          </div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination">
+        </div>
+      </swiper>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Swiper, SwiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        autoplay:{
+          delay:3000
+        }
+      }
+    }
+  }
 }
 </script>
 
